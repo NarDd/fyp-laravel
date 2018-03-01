418018bb953950fe23a0499b13fb951c193a7a63
NarDd <bernardng.kf@gmail.com>
2018-03-01 18:04:04 +0800

    added btnclick timestamp

--- app/Http/Controllers/API/EventController.php
+++ app/Http/Controllers/API/EventController.php
@@ -100,6 +100,7 @@ class EventController extends Controller
   public function postAttendance(Request $request){
     $attendance = Attendance::where('event_has_dates_id',$request->event_id)->where('user_id',$request->user_id)->first();
     $attendance->attendance = 1;
+    $attendance->pressed = $request->btn_click;
     $attendance->save();
     return response($attendance)->setStatusCode(200);
   }
